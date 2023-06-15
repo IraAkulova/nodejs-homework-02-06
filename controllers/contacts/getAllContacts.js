9ccad4481e37c7dd4254721fb462dbbd23cab66e
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-15 09:31:46 +0300

    Update getAllContacts.js

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,10 +3,10 @@ const {ctrlWrapper} = require("../../decorators");
 
 const get = async (req, res, next) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 10 } = req.query;
+  const { page = 1, limit = 10, ...query } = req.query;
   const skip = (page - 1) * limit;
 
-  const results = await Contact.find({owner, ...req.query}, '-createdAt -updatedAt', {skip, limit})
+  const results = await Contact.find({owner, ...query}, '-createdAt -updatedAt', {skip, limit})
     res.json({
       status: 'success',
       code: 200,
