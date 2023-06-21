a0aefb614ea2bd6c65c10cafe1673f94f3e340b7
Ira Akulova <ira.akulova2019@gmail.com>
2023-06-21 14:54:02 +0300

    06 done

--- api/authRouter.js
+++ api/authRouter.js
@@ -3,17 +3,21 @@ const authRouter = express.Router();
 
 const { validateBody } = require("../decorators");
 const authSchema = require("../schemas/authSchema");
+const verifyEmailSchema = require("../schemas/authSchema");
 const subscribtionSchema = require('../schemas/subscribtionSchema')
 const auth = require("../middlewares/auth");
 const { isValidId, upload } = require('../middlewares');
 
-const { register, login, logout, getCurrent, updateSubscription, updateAvatar } = require("../controllers/auth");
+const { register, verifyEmail, resendVerifyEmail, login, logout, getCurrent, updateSubscription, updateAvatar } = require("../controllers/auth");
 
 authRouter.post("/register", validateBody(authSchema), register);
+authRouter.get("/verify/:verificationToken", verifyEmail);
+authRouter.post("/verify", validateBody(verifyEmailSchema), resendVerifyEmail);
 authRouter.post("/login", validateBody(authSchema), login);
 authRouter.post("/logout", auth, logout);
 authRouter.get("/current", auth, validateBody(authSchema), getCurrent);
 authRouter.patch('/:id/subscription', auth, isValidId, validateBody(subscribtionSchema), updateSubscription);
-authRouter.patch("/avatars", auth, upload.single('avatar'), updateAvatar)
+authRouter.patch("/avatars", auth, upload.single('avatar'), updateAvatar);
+
 
 module.exports = authRouter;
--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,4 +1,6 @@
 const { register } = require('../auth/register');
+const { verifyEmail } = require('../auth/verifyEmail');
+const { resendVerifyEmail } = require('../auth/resendVerifyEmail');
 const { login } = require('../auth/login');
 const { logout } = require('../auth/logout');
 const { getCurrent } = require('../auth/gerCurrent');
@@ -7,6 +9,8 @@ const { updateAvatar } = require('../auth/updateAvatar');
 
 module.exports = {
   register,
+  verifyEmail,
+  resendVerifyEmail,
   login,
   logout,
   getCurrent,
